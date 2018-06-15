7f1d28ec88072721bdfe81114cd75441c4000bb3
McKenzi B <mckenziblanton@gmail.com>
2018-06-14 18:10:18 -0700

    Submitting Social Analytics

---
+++ Social_Analytics_HW/config3.py
@@ -0,0 +1,8 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key = "3okt1TpdU8OkZTAiHvO7rlyVM"
+consumer_secret = "1bDS9jBhhewlfGAaPGjjJsdmM8Fl0IM4olLAmSTTP967wKzz3N"
+access_token = "1004891445793132544-JrtAR1QnIdNa8tR4ub3y9boVVh7NjR"
+access_token_secret = "6s3bIjLKilGlpY3avuKeoEjvXvVkW3dyQlAz8W1CnqWSM"
+
+weather_api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
